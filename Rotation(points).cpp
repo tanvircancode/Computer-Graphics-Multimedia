09043af8d46dfbcb947eb59b081db58ce136f7e4
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2019-11-24 15:37:58 +0600

    Update Rotation(points).cpp

--- Rotation(points).cpp
+++ Rotation(points).cpp
@@ -12,13 +12,13 @@ void display()
 
    double x = 0.6f,y=0.0f;
 
-   //glVertex2f(0.0f,0.0f);
+   
    glVertex2f(x,y);
 
-   double ax = x*cos(10*180/3.1416)-y*sin(10*180/3.1416);
-   double ay = x*sin(10*180/3.1416)-y*cos(10*180/3.1416);
+   double ax = x*cos(10*180/3.1416)-y*sin(10*180/3.1416);   //Redian to degree
+   double ay = x*sin(10*180/3.1416)+y*cos(10*180/3.1416);
 
-  // glVertex2f(0.0f,0.0f);
+ 
    glVertex2f(ax,ay);
 
     glEnd();
